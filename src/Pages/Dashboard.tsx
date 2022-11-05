e2366f4080dc36999e33ada7002257f0bb4baee7
Stefan Nikolic <stefanikolic018@gmail.com>
2022-11-05 04:40:00 +0100

    google login succeeded when whitelisting the address, wrote todo list

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import useFirebaseContext from '../Context/Firebase/useFirebaseContext'
 
 export default function Dashboard() {
+  // TODO: 
+  // 1. Create register page, think of image or things like that
+  // 2. Think of removing email and password login, and also register if that's the case
+  // 3. Fix the UI
+  // 4. Refactor code and types
+  // 5. 
+  const { user } = useFirebaseContext();
+  console.log(user);
   return (
     <div>
       dashboard bro
