06fdce557d7f8593b0321d37d036d9be95d56dc7
Stefan Nikolic <stefanikolic018@gmail.com>
2023-01-09 16:45:38 +0100

    removing unused things from textsbar

--- src/Components/TextsBar/index.tsx
+++ src/Components/TextsBar/index.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 export default function index({ texts, selectedText, setSelectedText }: { texts: string[], selectedText: string | boolean, setSelectedText: React.Dispatch<React.SetStateAction<any>> }) {
 
+  // {text.last_edited}
   return <div className="w-4/12 h-[80%] z-20 p-2 border-2 dark:border-[#ffffffdb] border-zinc-700 rounded-xl bg-[#ffffff72] dark:bg-zinc-900  flex flex-col flex-wrap">
     {texts.map((text: any) =>
       <div key={text.name} className={`w-[45%] h-[15%] p-2 border bg-[#ffffff8d] dark:bg-zinc-800  text-black dark:text-white flex flex-col justify-start items-between rounded-xl cursor-pointer shadow-lg transition-all duration-200 hover:scale-105 ${selectedText === text.id ? 'border-orange-500' : ' border-white dark:border-zinc-700'}`} onClick={() => setSelectedText(text.id)}>
         <h1 className='font-bold text-sm'>{text.name}</h1>
         <hr />
         <p className='opacity-80 text-xs whitespace-pre-wrap truncate'>{text.body}</p>
-        {/* <p className='self-end'>{text.last_edited}</p> */}
       </div>)}
   </div>
 }
