fe3dacee6f495a6dcf992ea7f80888e37a6bd9bf
Stefan Nikolic <stefanikolic018@gmail.com>
2023-02-24 02:51:59 +0100

    started with action bar

--- src/Components/ActionBar/index.tsx
+++ src/Components/ActionBar/index.tsx
@@ -3,7 +3,7 @@ import React, { memo } from 'react';
 const index = () => {
   return (
     <div className='w-[35%] h-full z-20 p-2 border-2 dark:border-[#ffffffdb] border-zinc-700 rounded-xl bg-[#ffffff72] dark:bg-zinc-900  flex flex-col flex-wrap'>
-      index
+      <input type='text' />
     </div>
   );
 };
