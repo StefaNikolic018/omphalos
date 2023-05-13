abd8cb2fa888827d4792d3cfc37c0fc8b86ffc45
Stefan Nikolic <stefanikolic018@gmail.com>
2023-05-13 21:54:42 +0200

    lipstick: rounded tooltip

--- src/Components/Shared/Tooltip.tsx
+++ src/Components/Shared/Tooltip.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode, memo } from 'react'
 const Tooltip = ({ children, text }: { children: ReactNode; text: string }) => {
   return (
     <span className="group relative">
-      <span className="neutral-100 space-nowrap before:border-5 pointer-events-none absolute top-1 left-[65px] z-20 rounded-xl bg-zinc-900 px-2 py-1 text-xl text-neutral-100 opacity-0 transition before:absolute before:top-10 before:right-5 before:border-transparent before:border-r-zinc-900 before:content-[''] group-hover:opacity-100 dark:bg-neutral-100 dark:font-bold dark:text-zinc-900">
+      <span className="neutral-100 space-nowrap before:border-5 pointer-events-none absolute left-[65px] top-1 z-20 rounded-3xl bg-zinc-900 px-2 py-1 text-xl text-neutral-100 opacity-0 transition before:absolute before:right-5 before:top-10 before:border-transparent before:border-r-zinc-900 before:content-[''] group-hover:opacity-100 dark:bg-neutral-100 dark:font-bold dark:text-zinc-900">
         {text}
       </span>
 
